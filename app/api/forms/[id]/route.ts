0ee3ca35104b23e1036c25bfd6bf0283f991bb52
divyang-chhantbar <divyangchhantbar@gmail.com>
2026-02-12 22:03:06 +0530

    feat: add DELETE endpoint for form deletion with user authentication and error handling

--- app/api/forms/[id]/route.ts
+++ app/api/forms/[id]/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 /*
@@ -8,9 +9,12 @@ import { NextRequest, NextResponse } from "next/server";
     return the response that are attached with that particular formId
 */
 
-export async function GET(req : NextRequest,{ params }: { params: Promise<{ id: string }> }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
   try {
-    const {id} = await params;
+    const { id } = await params;
     // console.log("Received form ID:", id);
 
     if (!id || id.trim().length === 0) {
@@ -24,20 +28,14 @@ export async function GET(req : NextRequest,{ params }: { params: Promise<{ id: 
 
     const data = await prisma.forms.findFirst({
       where: {
-        OR : [
-          { id: id },
-          { slug: id },
-        ]
+        OR: [{ id: id }, { slug: id }],
       },
     });
-    console.log("data coming from db : ",data);
-    
+    console.log("data coming from db : ", data);
+
     if (!data) {
-  return NextResponse.json(
-    { error: "Form not found" },
-    { status: 404 }
-  );
-}
+      return NextResponse.json({ error: "Form not found" }, { status: 404 });
+    }
 
     return NextResponse.json(
       {
@@ -46,7 +44,58 @@ export async function GET(req : NextRequest,{ params }: { params: Promise<{ id: 
       },
       { status: 200 },
     );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      { status: 500 },
+    );
+  }
+}
 
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    // user authentication
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { id } = await params;
+    if (!id || id.trim().length === 0) {
+      return NextResponse.json(
+        { error: "Form ID is required and cannot be empty" },
+        { status: 400 },
+      );
+    }
+
+    const form = await prisma.forms.findUnique({
+      where: {
+        id: id,
+      },
+      select: { userId: true },
+    });
+
+    if (!form) {
+      return NextResponse.json({ error: "Form not found" }, { status: 404 });
+    }
+
+    if (form.userId !== userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const deletedForm = await prisma.forms.delete({
+      where: { id: id },
+    });
+
+    return NextResponse.json(
+      { success: true, data: deletedForm },
+      { status: 200 },
+    );
   } catch (error) {
     return NextResponse.json(
       {
